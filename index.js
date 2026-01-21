98ebd07031cb8711d492a95ca26098002745b0c6
Vinicius-Correia-Moraes-Cardoso <deariedarling2022@gmail.com>
2026-01-21 00:14:02 -0300

    Added the function that let's the player chose between rock paper and scissor.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+var computerScore = 0;
+var humanScore = 0;
+
+
 function getComputerChoice(){
 let num = (Math.random())
 if (num <= .3){
     return "rock"
-}else if (num >=.4 && num <=.6){
+} else if (num >=.4 && num <=.6){
     return "paper"
 } else {
     return "scissor"
 }
 }
 
+function getHumanChoice(){
+let choice = prompt("Choose between rock, paper or scissor.", "rock")
+if (choice === "rock"){
+    return "rock"
+ }else if (choice === "paper"){
+    return "paper"
+} else if (choice === "scissor"){
+    return "scissor"
+}
+}
 
-console.log(getComputerChoice())
+console.log(getHumanChoice())
