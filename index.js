bc8682233db0b1bd3a525e7c15f5b5985caf2d21
Vinicius-Correia-Moraes-Cardoso <deariedarling2022@gmail.com>
2026-01-20 23:46:47 -0300

    Removed html document boilerplate as it was uneces sary and added function that makes the computer chose between rock paper or scissors.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world!")
+function getComputerChoice(){
+let num = (Math.random())
+if (num <= .3){
+    return "rock"
+}else if (num >=.4 && num <=.6){
+    return "paper"
+} else {
+    return "scissor"
+}
+}
+
+
+console.log(getComputerChoice())
